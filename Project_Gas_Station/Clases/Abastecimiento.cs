d71c466fa01b11a55202aea66b6834ea5f9f591e
alex-pereira59 <alex59@umes.edu.gt>
2024-05-27 21:00:31 -0600

    Diseño FormGasolinera y modificaciones clase Abastecimiento.

--- Project_Gas_Station/Clases/Abastecimiento.cs
+++ Project_Gas_Station/Clases/Abastecimiento.cs
@@ -12,23 +12,23 @@ namespace Project_Gas_Station.Clases
         private DateTime fechaHora;
         private double cantidad;
         private double precioPorLitro;
-        private bool esPrepago;
+        private string tipoDespacho;
         private string nombreCliente;
 
         public int IdBomba { get => idBomba; set => idBomba = value; }
         public DateTime FechaHora { get => fechaHora; set => fechaHora = value; }
         public double Cantidad { get => cantidad; set => cantidad = value; }
         public double PrecioPorLitro { get => precioPorLitro; set => precioPorLitro = value; }
-        public bool EsPrepago { get => esPrepago; set => esPrepago = value; }
+        public string EsPrepago { get => tipoDespacho; set => tipoDespacho = value; }
         public string NombreCliente { get => nombreCliente; set => nombreCliente = value; }
 
-        public Abastecimiento(int idBomba, double cantidad, double precioPorLitro, bool esPrepago, string nombreCliente)
+        public Abastecimiento(int idBomba, double cantidad, double precioPorLitro, string tipoDespacho, string nombreCliente)
         {
             this.IdBomba = idBomba;
             this.FechaHora = DateTime.Now;
             this.Cantidad = cantidad;
             this.PrecioPorLitro = precioPorLitro;
-            this.EsPrepago = esPrepago;
+            this.EsPrepago = tipoDespacho;
             this.NombreCliente = nombreCliente;
         }
     }
--- Project_Gas_Station/GUI/AdminForm.Designer.cs
+++ Project_Gas_Station/GUI/AdminForm.Designer.cs
@@ -28,12 +28,363 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "AdminForm";
+            tabControl1 = new TabControl();
+            tabPageDispensar = new TabPage();
+            tabPageEstadisticas = new TabPage();
+            panel1 = new Panel();
+            panel2 = new Panel();
+            panel3 = new Panel();
+            panel4 = new Panel();
+            panel5 = new Panel();
+            textBox1 = new TextBox();
+            textBox2 = new TextBox();
+            textBox3 = new TextBox();
+            textBox4 = new TextBox();
+            button1 = new Button();
+            button2 = new Button();
+            radioButton1 = new RadioButton();
+            radioButton2 = new RadioButton();
+            radioButton3 = new RadioButton();
+            radioButton4 = new RadioButton();
+            radioButton5 = new RadioButton();
+            radioButton6 = new RadioButton();
+            button3 = new Button();
+            radioButton7 = new RadioButton();
+            radioButton8 = new RadioButton();
+            button4 = new Button();
+            progressBar1 = new ProgressBar();
+            progressBar2 = new ProgressBar();
+            progressBar3 = new ProgressBar();
+            progressBar4 = new ProgressBar();
+            tabControl1.SuspendLayout();
+            tabPageDispensar.SuspendLayout();
+            panel2.SuspendLayout();
+            panel3.SuspendLayout();
+            panel4.SuspendLayout();
+            panel5.SuspendLayout();
+            SuspendLayout();
+            // 
+            // tabControl1
+            // 
+            tabControl1.Controls.Add(tabPageDispensar);
+            tabControl1.Controls.Add(tabPageEstadisticas);
+            tabControl1.Location = new Point(12, 12);
+            tabControl1.Name = "tabControl1";
+            tabControl1.SelectedIndex = 0;
+            tabControl1.Size = new Size(1240, 656);
+            tabControl1.TabIndex = 0;
+            // 
+            // tabPageDispensar
+            // 
+            tabPageDispensar.Controls.Add(panel5);
+            tabPageDispensar.Controls.Add(panel4);
+            tabPageDispensar.Controls.Add(panel3);
+            tabPageDispensar.Controls.Add(panel2);
+            tabPageDispensar.Controls.Add(panel1);
+            tabPageDispensar.Location = new Point(4, 34);
+            tabPageDispensar.Name = "tabPageDispensar";
+            tabPageDispensar.Padding = new Padding(3);
+            tabPageDispensar.Size = new Size(1232, 618);
+            tabPageDispensar.TabIndex = 0;
+            tabPageDispensar.Text = "Disepensar";
+            tabPageDispensar.UseVisualStyleBackColor = true;
+            // 
+            // tabPageEstadisticas
+            // 
+            tabPageEstadisticas.Location = new Point(4, 34);
+            tabPageEstadisticas.Name = "tabPageEstadisticas";
+            tabPageEstadisticas.Padding = new Padding(3);
+            tabPageEstadisticas.Size = new Size(1232, 618);
+            tabPageEstadisticas.TabIndex = 1;
+            tabPageEstadisticas.Text = "Estadisticas";
+            tabPageEstadisticas.UseVisualStyleBackColor = true;
+            // 
+            // panel1
+            // 
+            panel1.BackColor = Color.Turquoise;
+            panel1.Location = new Point(6, 6);
+            panel1.Name = "panel1";
+            panel1.Size = new Size(1218, 96);
+            panel1.TabIndex = 0;
+            // 
+            // panel2
+            // 
+            panel2.Controls.Add(progressBar1);
+            panel2.Controls.Add(radioButton2);
+            panel2.Controls.Add(radioButton1);
+            panel2.Controls.Add(button1);
+            panel2.Controls.Add(textBox1);
+            panel2.Location = new Point(6, 108);
+            panel2.Name = "panel2";
+            panel2.Size = new Size(300, 504);
+            panel2.TabIndex = 1;
+            // 
+            // panel3
+            // 
+            panel3.Controls.Add(progressBar2);
+            panel3.Controls.Add(radioButton4);
+            panel3.Controls.Add(radioButton3);
+            panel3.Controls.Add(button2);
+            panel3.Controls.Add(textBox2);
+            panel3.Location = new Point(312, 108);
+            panel3.Name = "panel3";
+            panel3.Size = new Size(300, 504);
+            panel3.TabIndex = 2;
+            // 
+            // panel4
+            // 
+            panel4.Controls.Add(progressBar3);
+            panel4.Controls.Add(button3);
+            panel4.Controls.Add(radioButton6);
+            panel4.Controls.Add(radioButton5);
+            panel4.Controls.Add(textBox3);
+            panel4.Location = new Point(618, 108);
+            panel4.Name = "panel4";
+            panel4.Size = new Size(300, 504);
+            panel4.TabIndex = 3;
+            // 
+            // panel5
+            // 
+            panel5.Controls.Add(progressBar4);
+            panel5.Controls.Add(button4);
+            panel5.Controls.Add(radioButton8);
+            panel5.Controls.Add(radioButton7);
+            panel5.Controls.Add(textBox4);
+            panel5.Location = new Point(924, 108);
+            panel5.Name = "panel5";
+            panel5.Size = new Size(300, 504);
+            panel5.TabIndex = 4;
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(37, 32);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(219, 31);
+            textBox1.TabIndex = 0;
+            // 
+            // textBox2
+            // 
+            textBox2.Location = new Point(48, 32);
+            textBox2.Name = "textBox2";
+            textBox2.Size = new Size(208, 31);
+            textBox2.TabIndex = 0;
+            // 
+            // textBox3
+            // 
+            textBox3.Location = new Point(54, 32);
+            textBox3.Name = "textBox3";
+            textBox3.Size = new Size(201, 31);
+            textBox3.TabIndex = 0;
+            // 
+            // textBox4
+            // 
+            textBox4.Location = new Point(52, 32);
+            textBox4.Name = "textBox4";
+            textBox4.Size = new Size(188, 31);
+            textBox4.TabIndex = 0;
+            // 
+            // button1
+            // 
+            button1.Location = new Point(88, 201);
+            button1.Name = "button1";
+            button1.Size = new Size(112, 34);
+            button1.TabIndex = 1;
+            button1.Text = "button1";
+            button1.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            button2.Location = new Point(92, 201);
+            button2.Name = "button2";
+            button2.Size = new Size(112, 34);
+            button2.TabIndex = 1;
+            button2.Text = "button2";
+            button2.UseVisualStyleBackColor = true;
+            // 
+            // radioButton1
+            // 
+            radioButton1.AutoSize = true;
+            radioButton1.Location = new Point(37, 105);
+            radioButton1.Name = "radioButton1";
+            radioButton1.Size = new Size(141, 29);
+            radioButton1.TabIndex = 2;
+            radioButton1.TabStop = true;
+            radioButton1.Text = "radioButton1";
+            radioButton1.UseVisualStyleBackColor = true;
+            // 
+            // radioButton2
+            // 
+            radioButton2.AutoSize = true;
+            radioButton2.Location = new Point(37, 140);
+            radioButton2.Name = "radioButton2";
+            radioButton2.Size = new Size(141, 29);
+            radioButton2.TabIndex = 3;
+            radioButton2.TabStop = true;
+            radioButton2.Text = "radioButton2";
+            radioButton2.UseVisualStyleBackColor = true;
+            // 
+            // radioButton3
+            // 
+            radioButton3.AutoSize = true;
+            radioButton3.Location = new Point(48, 105);
+            radioButton3.Name = "radioButton3";
+            radioButton3.Size = new Size(141, 29);
+            radioButton3.TabIndex = 2;
+            radioButton3.TabStop = true;
+            radioButton3.Text = "radioButton3";
+            radioButton3.UseVisualStyleBackColor = true;
+            // 
+            // radioButton4
+            // 
+            radioButton4.AutoSize = true;
+            radioButton4.Location = new Point(48, 140);
+            radioButton4.Name = "radioButton4";
+            radioButton4.Size = new Size(141, 29);
+            radioButton4.TabIndex = 3;
+            radioButton4.TabStop = true;
+            radioButton4.Text = "radioButton4";
+            radioButton4.UseVisualStyleBackColor = true;
+            // 
+            // radioButton5
+            // 
+            radioButton5.AutoSize = true;
+            radioButton5.Location = new Point(54, 105);
+            radioButton5.Name = "radioButton5";
+            radioButton5.Size = new Size(141, 29);
+            radioButton5.TabIndex = 1;
+            radioButton5.TabStop = true;
+            radioButton5.Text = "radioButton5";
+            radioButton5.UseVisualStyleBackColor = true;
+            // 
+            // radioButton6
+            // 
+            radioButton6.AutoSize = true;
+            radioButton6.Location = new Point(54, 140);
+            radioButton6.Name = "radioButton6";
+            radioButton6.Size = new Size(141, 29);
+            radioButton6.TabIndex = 2;
+            radioButton6.TabStop = true;
+            radioButton6.Text = "radioButton6";
+            radioButton6.UseVisualStyleBackColor = true;
+            // 
+            // button3
+            // 
+            button3.Location = new Point(97, 201);
+            button3.Name = "button3";
+            button3.Size = new Size(112, 34);
+            button3.TabIndex = 3;
+            button3.Text = "button3";
+            button3.UseVisualStyleBackColor = true;
+            // 
+            // radioButton7
+            // 
+            radioButton7.AutoSize = true;
+            radioButton7.Location = new Point(52, 105);
+            radioButton7.Name = "radioButton7";
+            radioButton7.Size = new Size(141, 29);
+            radioButton7.TabIndex = 1;
+            radioButton7.TabStop = true;
+            radioButton7.Text = "radioButton7";
+            radioButton7.UseVisualStyleBackColor = true;
+            // 
+            // radioButton8
+            // 
+            radioButton8.AutoSize = true;
+            radioButton8.Location = new Point(52, 140);
+            radioButton8.Name = "radioButton8";
+            radioButton8.Size = new Size(141, 29);
+            radioButton8.TabIndex = 2;
+            radioButton8.TabStop = true;
+            radioButton8.Text = "radioButton8";
+            radioButton8.UseVisualStyleBackColor = true;
+            // 
+            // button4
+            // 
+            button4.Location = new Point(91, 201);
+            button4.Name = "button4";
+            button4.Size = new Size(112, 34);
+            button4.TabIndex = 3;
+            button4.Text = "button4";
+            button4.UseVisualStyleBackColor = true;
+            // 
+            // progressBar1
+            // 
+            progressBar1.Location = new Point(88, 289);
+            progressBar1.Name = "progressBar1";
+            progressBar1.Size = new Size(150, 34);
+            progressBar1.TabIndex = 5;
+            // 
+            // progressBar2
+            // 
+            progressBar2.Location = new Point(73, 289);
+            progressBar2.Name = "progressBar2";
+            progressBar2.Size = new Size(150, 34);
+            progressBar2.TabIndex = 4;
+            // 
+            // progressBar3
+            // 
+            progressBar3.Location = new Point(78, 289);
+            progressBar3.Name = "progressBar3";
+            progressBar3.Size = new Size(150, 34);
+            progressBar3.TabIndex = 4;
+            // 
+            // progressBar4
+            // 
+            progressBar4.Location = new Point(80, 289);
+            progressBar4.Name = "progressBar4";
+            progressBar4.Size = new Size(150, 34);
+            progressBar4.TabIndex = 4;
+            // 
+            // AdminForm
+            // 
+            AutoScaleDimensions = new SizeF(10F, 25F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(1259, 680);
+            Controls.Add(tabControl1);
+            Name = "AdminForm";
+            Text = "Gasolinera";
+            tabControl1.ResumeLayout(false);
+            tabPageDispensar.ResumeLayout(false);
+            panel2.ResumeLayout(false);
+            panel2.PerformLayout();
+            panel3.ResumeLayout(false);
+            panel3.PerformLayout();
+            panel4.ResumeLayout(false);
+            panel4.PerformLayout();
+            panel5.ResumeLayout(false);
+            panel5.PerformLayout();
+            ResumeLayout(false);
         }
 
         #endregion
+
+        private TabControl tabControl1;
+        private TabPage tabPageDispensar;
+        private Panel panel5;
+        private Panel panel4;
+        private Panel panel3;
+        private TextBox textBox2;
+        private Panel panel2;
+        private TextBox textBox1;
+        private Panel panel1;
+        private TabPage tabPageEstadisticas;
+        private Button button4;
+        private RadioButton radioButton8;
+        private RadioButton radioButton7;
+        private TextBox textBox4;
+        private Button button3;
+        private RadioButton radioButton6;
+        private RadioButton radioButton5;
+        private TextBox textBox3;
+        private RadioButton radioButton4;
+        private RadioButton radioButton3;
+        private Button button2;
+        private RadioButton radioButton2;
+        private RadioButton radioButton1;
+        private Button button1;
+        private ProgressBar progressBar4;
+        private ProgressBar progressBar3;
+        private ProgressBar progressBar2;
+        private ProgressBar progressBar1;
     }
 }
